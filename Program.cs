44eaa50f013ee66cb6b7bec83aa769a0a217cc7e
5zam <zuwina.s.aljabri@gmail.com>
2025-04-16 20:25:09 +0400

    تنظيف الملفات الغير ضرورية وإضافة gitignore

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Problem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+                // user inputs:
+                Console.Write("Enter number of white balls: ");
+                int white = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter number of black balls: ");
+                int black = int.Parse(Console.ReadLine());
+
+                //start from first level ( default value )
+                int level = 1;
+                //start from white ball
+                bool isWhite = true;
+
+                while (true)
+                {
+                    //first check white balls
+                    if (isWhite)
+                    {
+                        if (white >= level)
+                        {
+                            white -= level; 
+                            level++;        // move to next level
+                            isWhite = false; // change color to black
+                        }
+                        else
+                        {
+                            break; //No enough white balls
+                        }
+                    }
+                //first check white balls
+                    else
+                {
+                        if (black >= level)
+                        {
+                            black -= level;
+                            level++;
+                            isWhite = true;
+                        }
+                        else
+                        {
+                            break; //No enough black balls
+                    }
+                    }
+                }
+
+                // maximum level can reach 
+                Console.WriteLine("Max level of the Triangle is : " + (level - 1));
+            }
+        }
+
+    }
+
